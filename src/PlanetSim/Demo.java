81ce35b2b30611be71bd9d35f8d022b56633abc1
dnhlms@gmail.com <dnhlms@gmail.com>
2014-11-15 19:18:12 -0500

    cmd line parsing and put the values or the defaults into settings

--- src/PlanetSim/Demo.java
+++ src/PlanetSim/Demo.java
@@ -18,6 +18,28 @@ public class Demo
     public static void main(final String[] args)
     {
         init();
+        settings.setTemporalPrecision(100);
+        settings.setDatastoragePrecision(7);
+        settings.setGeographicPrecision(100);
+    	for (int i = 0; i < args.length; i++)
+    	{
+    		if (args[i].equals("-t"))
+    	    	//-t #: The temporal precision of the temperature data to be stored, as an integer percentage of the number 
+    	        //of time periods saved versus the number computed. The default is 100%; that is, all computed values 
+    	        //should be stored. 
+    			settings.setTemporalPrecision(Integer.parseInt(args[i+1]));
+    		else if (args[i].equals("-g"))
+    	    	//-g #: The geographic precision (sampling rate) of the temperature data to be stored, as an integer 
+    	        //percentage of the number of grid cells saved versus the number simulated. The default is 100%; that 
+    	        //is, a value is stored for each grid cell.
+    			settings.setGeographicPrecision(Integer.parseInt(args[i+1]));
+    		else if (args[i].equals("-p"))
+    	    	//-p #: The precision of the data to be stored, in decimal digits after the decimal point. The default is 
+    	        //to use the number of digits storable in a normalized float variable. The maximum is the number of digits
+    	        //storable in a normalized double variable. The minimum is zero.
+    			settings.setDatastoragePrecision(Integer.parseInt(args[i+1]));
+    			
+    	}
         startUI();
         startSimulation();
         startMetrics();
@@ -37,6 +59,9 @@ public class Demo
 
     private static void startSimulation()
     {
+    	//probably don't need to send in settings anymore since the UI will put them on the
+    	//bus and these will end up in here eventually.  there isn't a need to seed this data
+    	//anymore since it is an incomplete set of settings
         new SimulationEngineDaemon(eventBus, settings);
     }
 
@@ -47,6 +72,7 @@ public class Demo
             @Override
             public void run()
             {
+            	//these are required because the cmd line has values that have to make it into the UI
                 new SimulationUI(eventBus, settings);
             }
         });
